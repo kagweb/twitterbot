0f9ade9ba8e3528f5191915519ee295e6c0c9f34
Ichiro Kimura <kag.web@gmail.com>
2014-12-24 01:44:47 +0900

    複数リスト問題に対処

--- list.rb
+++ list.rb
@@ -10,13 +10,16 @@ class List
   include TwitterCapybara
 
   SEARCH_WORDS = '#アニメ好きな人RT'
-  LIST_WORD = 'アニメ好きなクラスタ'
+  LIST_NAME = 'アニメ好きなクラスタ'
+  # SEARCH_WORD = '萩原雪歩'
+  # SEARCH_WORDS = "(\##{SEARCH_WORD}誕生祭 OR \##{SEARCH_WORD}生誕祭 OR \##{SEARCH_WORD}誕生祭2014 OR \##{SEARCH_WORD}生誕祭2014)"
+  # LIST_NAME = '雪歩クラスタ'
   SEARCH_COUNT = 200
-  LIST_IN_COUNT = 3
+  LIST_IN_COUNT = 20
 
   Bot.login(Configuration.bot_screen_name, Configuration.bot_password)
   Bot.search("#{SEARCH_WORDS} #RTした人全員フォローする -RT -#相互 -#相互希望 -#相互限定 -#相互フォロー")
-  Bot.list_in(count = LIST_IN_COUNT, word = LIST_WORD)
+  Bot.list_in(count = LIST_IN_COUNT, list_name = LIST_NAME)
 
   puts 'list in task done!'
 end
